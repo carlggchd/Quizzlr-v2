7f3efd7bc6ee162b9e9a225c25638c3592935aeb
Carl <carlpropriate@gmail.com>
2023-03-02 11:57:22 +0800

    admin account is working and quiz page behaves differently if admin account is being used

--- main/QUIZ/quizPage.php
+++ main/QUIZ/quizPage.php
@@ -12,123 +12,164 @@
           <h1>QUIZZLR</h1>
           <p>The ultimate quiz randomizer</p>
           <div class="welcome-msg">
-              <?php
-              session_start();
-              include_once '../../../carlRandomizer/config/dbcon.php';
-              if(isset($_SESSION['username'])){
-                echo "Welcome, ".$_SESSION['username']."!";
-              }
-              ?>
-          </div>
-        <nav class="navbar"> 
-          <ul class="nav-menu"> 
-            <li class="nav-item">
-              <a href="../../../carlRandomizer/index.php" class="nav-link">Home</a>
-            </li>
-            <li class="nav-item">
-              <a href="../../../carlRandomizer/main//QUIZ/stud_info-register.php" class="nav-link">Register</a>
-            </li>
-            <li class="nav-item">
-              <a href="" class="nav-link">About (under construction)</a>
-            </li>
-            <li class="nav-item">
-              <a href="../../../carlRandomizer/main/LOGIN/logout.php" class="nav-link">Logout</a>
-            </li>
-          </ul>
-          <div class="hamburger">
-            <span class="bar"></span>
-            <span class="bar"></span>
-            <span class="bar"></span>
-          </div>
-         
-        </nav>
+    <?php
+    session_start();
+    include_once '../../../carlRandomizer/config/dbcon.php';
+    $student_id = $_SESSION['student_id'];
+    $result = mysqli_query($conn, "SELECT admin_status FROM tbl_login WHERE student_id = $student_id");
+    $statusCheck = mysqli_fetch_assoc($result);
+    if(isset($_SESSION['username'])){
+      echo "Welcome, ".$_SESSION['username']."!";
+    }
+    ?>
+  </div>
+  <nav class="navbar"> 
+    <ul class="nav-menu"> 
+      <li class="nav-item">
+        <a href="../../../carlRandomizer/index.php" class="nav-link">Home</a>
+      </li>
+      <li class="nav-item">
+        <a href="../../../carlRandomizer/main//QUIZ/stud_info-register.php" class="nav-link">Register</a>
+      </li>
+      <li class="nav-item">
+        <a href="" class="nav-link">About (under construction)</a>
+      </li>
+      <li class="nav-item">
+        <a href="../../../carlRandomizer/main/LOGIN/logout.php" class="nav-link">Logout</a>
+      </li>
+    </ul>
+    <div class="hamburger">
+      <span class="bar"></span>
+      <span class="bar"></span>
+      <span class="bar"></span>
+    </div>
+  </nav>
+</div>
+
+<div class="row">
+
+    <div class="card">
+         <h2>Quiz Category 1</h2>
+         <p>Description for quiz category 1</p>
+         <?php
+            include_once '../../../carlRandomizer/config/dbcon.php';
+            $student_id = $_SESSION['student_id'];
+            $result = mysqli_query($conn, "SELECT * FROM tbl_student_info WHERE student_id='$student_id'");
+            if (mysqli_num_rows($result) == 0) {
+               echo "<p>Please register your information before taking this quiz.</p>";
+            } 
+            else {
+               if($statusCheck['admin_status'] == 1){
+               echo "<a href='../../../carlRandomizer/main/QUIZ/quizCategories/uploadQuiz.php'><button>Upload Quiz</button></a>";
+               } else {
+               echo "<a href='../../../carlRandomizer/main/QUIZ/quizCategories/quizCategory1.php'><button>Start Quiz</button></a>";
+               }
+            }
+         ?>         
       </div>
-      <div class="row">
-         <div class="card">
-            <h2>Quiz Category 1</h2>
-            <p>Description for quiz category 1</p>
-            <?php
-               include_once '../../../carlRandomizer/config/dbcon.php';
-               $student_id = $_SESSION['student_id'];
-               $result = mysqli_query($conn, "SELECT * FROM tbl_student_info WHERE student_id='$student_id'");
-               if (mysqli_num_rows($result) == 0) {
-                     echo "<p>Please register your information before taking this quiz.</p>";
+
+      <div class="card">
+         <h2>Quiz Category 2</h2>
+         <p>Description for quiz category 1</p>
+         <?php
+            include_once '../../../carlRandomizer/config/dbcon.php';
+            $student_id = $_SESSION['student_id'];
+            $result = mysqli_query($conn, "SELECT * FROM tbl_student_info WHERE student_id='$student_id'");
+            if (mysqli_num_rows($result) == 0) {
+               echo "<p>Please register your information before taking this quiz.</p>";
+            } 
+            else {
+               if($statusCheck['admin_status'] == 1){
+               echo "<a href='../../../carlRandomizer/main/QUIZ/quizCategories/uploadQuiz.php'><button>Upload Quiz</button></a>";
                } else {
-                     echo "<a href='../../../carlRandomizer/main/QUIZ/quizCategories/quizCategory1.php'><button>Start Quiz</button></a>";
+               echo "<a href='../../../carlRandomizer/main/QUIZ/quizCategories/quizCategory2.php'><button>Start Quiz</button></a>";
                }
-            ?>         
-         </div>
-         <div class="card">
-            <h2>Quiz Category 2</h2>
-            <p>Description for quiz category 2</p>
-            <?php
-               include_once '../../../carlRandomizer/config/dbcon.php';
-               $student_id = $_SESSION['student_id'];
-               $result = mysqli_query($conn, "SELECT * FROM tbl_student_info WHERE student_id='$student_id'");
-               if (mysqli_num_rows($result) == 0) {
-                     echo "<p>Please register your information before taking this quiz.</p>";
-               } else {
-                     echo "<a href='../../../carlRandomizer/main/QUIZ/quizCategories/quizCategory2.php'><button>Start Quiz</button></a>";
-               }
-            ?>    
-         </div>
-         <div class="card">
-            <h2>Quiz Category 3</h2>
-            <p>Description for quiz category 3</p>
-            <?php
-               include_once '../../../carlRandomizer/config/dbcon.php';
-               $student_id = $_SESSION['student_id'];
-               $result = mysqli_query($conn, "SELECT * FROM tbl_student_info WHERE student_id='$student_id'");
-               if (mysqli_num_rows($result) == 0) {
-                     echo "<p>Please register your information before taking this quiz.</p>";
-               } else {
-                     echo "<a href='../../../carlRandomizer/main/QUIZ/quizCategories/quizCategory3.php'><button>Start Quiz</button></a>";
-               }
-            ?> 
-         </div>
-         <div class="card">
-            <h2>Quiz Category 4</h2>
-            <p>Description for quiz category 4</p>
-            <?php
-               include_once '../../../carlRandomizer/config/dbcon.php';
-               $student_id = $_SESSION['student_id'];
-               $result = mysqli_query($conn, "SELECT * FROM tbl_student_info WHERE student_id='$student_id'");
-               if (mysqli_num_rows($result) == 0) {
-                     echo "<p>Please register your information before taking this quiz.</p>";
-               } else {
-                     echo "<a href='../../../carlRandomizer/main/QUIZ/quizCategories/quizCategory4.php'><button>Start Quiz</button></a>";
-               }
-            ?> 
-         </div>
-         <div class="card">
-            <h2>Quiz Category 5</h2>
-            <p>Description for quiz category 5</p>
-            <?php
-               include_once '../../../carlRandomizer/config/dbcon.php';
-               $student_id = $_SESSION['student_id'];
-               $result = mysqli_query($conn, "SELECT * FROM tbl_student_info WHERE student_id='$student_id'");
-               if (mysqli_num_rows($result) == 0) {
-                     echo "<p>Please register your information before taking this quiz.</p>";
-               } else {
-                     echo "<a href='../../../carlRandomizer/main/QUIZ/quizCategories/quizCategory5.php'><button>Start Quiz</button></a>";
-               }
-            ?> 
-         </div>
-         <div class="card">
-            <h2>Quiz Category 6</h2>
-            <p>Description for quiz category 6</p>
-            <?php
-               include_once '../../../carlRandomizer/config/dbcon.php';
-               $student_id = $_SESSION['student_id'];
-               $result = mysqli_query($conn, "SELECT * FROM tbl_student_info WHERE student_id='$student_id'");
-               if (mysqli_num_rows($result) == 0) {
-                     echo "<p>Please register your information before taking this quiz.</p>";
-               } else {
-                     echo "<a href='../../../carlRandomizer/main/QUIZ/quizCategories/quizCategory6.php'><button>Start Quiz</button></a>";
-               }
-            ?> 
-         </div>
+            }
+         ?>         
       </div>
+
+      <div class="card">
+         <h2>Quiz Category 3</h2>
+         <p>Description for quiz category 1</p>
+         <?php
+            include_once '../../../carlRandomizer/config/dbcon.php';
+            $student_id = $_SESSION['student_id'];
+            $result = mysqli_query($conn, "SELECT * FROM tbl_student_info WHERE student_id='$student_id'");
+            if (mysqli_num_rows($result) == 0) {
+               echo "<p>Please register your information before taking this quiz.</p>";
+            } 
+            else {
+               if($statusCheck['admin_status'] == 1){
+               echo "<a href='../../../carlRandomizer/main/QUIZ/quizCategories/uploadQuiz.php'><button>Upload Quiz</button></a>";
+               } else {
+               echo "<a href='../../../carlRandomizer/main/QUIZ/quizCategories/quizCategory3.php'><button>Start Quiz</button></a>";
+               }
+            }
+         ?>         
+      </div>
+
+      <div class="card">
+         <h2>Quiz Category 4</h2>
+         <p>Description for quiz category 1</p>
+         <?php
+            include_once '../../../carlRandomizer/config/dbcon.php';
+            $student_id = $_SESSION['student_id'];
+            $result = mysqli_query($conn, "SELECT * FROM tbl_student_info WHERE student_id='$student_id'");
+            if (mysqli_num_rows($result) == 0) {
+               echo "<p>Please register your information before taking this quiz.</p>";
+            } 
+            else {
+               if($statusCheck['admin_status'] == 1){
+               echo "<a href='../../../carlRandomizer/main/QUIZ/quizCategories/uploadQuiz.php'><button>Upload Quiz</button></a>";
+               } else {
+               echo "<a href='../../../carlRandomizer/main/QUIZ/quizCategories/quizCategory4.php'><button>Start Quiz</button></a>";
+               }
+            }
+         ?>         
+      </div>
+
+      <div class="card">
+         <h2>Quiz Category 5</h2>
+         <p>Description for quiz category 1</p>
+         <?php
+            include_once '../../../carlRandomizer/config/dbcon.php';
+            $student_id = $_SESSION['student_id'];
+            $result = mysqli_query($conn, "SELECT * FROM tbl_student_info WHERE student_id='$student_id'");
+            if (mysqli_num_rows($result) == 0) {
+               echo "<p>Please register your information before taking this quiz.</p>";
+            } 
+            else {
+               if($statusCheck['admin_status'] == 1){
+               echo "<a href='../../../carlRandomizer/main/QUIZ/quizCategories/uploadQuiz.php'><button>Upload Quiz</button></a>";
+               } else {
+               echo "<a href='../../../carlRandomizer/main/QUIZ/quizCategories/quizCategory5.php'><button>Start Quiz</button></a>";
+               }
+            }
+         ?>         
+      </div>
+
+      <div class="card">
+         <h2>Quiz Category 6</h2>
+         <p>Description for quiz category 1</p>
+         <?php
+            include_once '../../../carlRandomizer/config/dbcon.php';
+            $student_id = $_SESSION['student_id'];
+            $result = mysqli_query($conn, "SELECT * FROM tbl_student_info WHERE student_id='$student_id'");
+            if (mysqli_num_rows($result) == 0) {
+               echo "<p>Please register your information before taking this quiz.</p>";
+            } 
+            else {
+               if($statusCheck['admin_status'] == 1){
+               echo "<a href='../../../carlRandomizer/main/QUIZ/quizCategories/uploadQuiz.php'><button>Upload Quiz</button></a>";
+               } else {
+               echo "<a href='../../../carlRandomizer/main/QUIZ/quizCategories/quizCategory6.php'><button>Start Quiz</button></a>";
+               }
+            }
+         ?>         
+      </div>
+
+      </div>
+
       <script>
         const hamburger = document.querySelector(".hamburger");
         const navMenu = document.querySelector(".nav-menu");
